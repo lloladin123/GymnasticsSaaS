10ab1d75b797989eb176526b50244c75ca0536dd
lloladin123 <blackiealexander@yahoo.dk>
2025-05-11 15:30:16 +0000

    fixed: Docker compiling issues feat: CtaHero and video

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Logo from "@/components/Logo";
 import NavBar from "@/components/NavBar";
 import HeaderSideContent from "@/components/HeaderSideContent";
+import CtaJoin from "@/components/CtaJoin";
 
 export default function Home() {
   return (
@@ -15,6 +16,26 @@ export default function Home() {
         <NavBar></NavBar>
         <HeaderSideContent></HeaderSideContent>
       </header>
+      <main className="p-4">
+        <CtaJoin variants="hero"></CtaJoin>
+        <div className="w-full max-h-screen overflow-hidden">
+          <video
+            width="100%"
+            height="auto"
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="object-cover"
+          >
+            <source
+              src="/videos/rgf_hovedfilm.mp4"
+              type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+            />
+            Your browser does not support your video tag.
+          </video>
+        </div>
+      </main>
     </>
   );
 }
