f6014808d1c4408895b0623ee85d3fcfc7d5d16c
lloladin123 <blackiealexnader@yahoo.dk>
2025-05-17 19:31:26 +0200

    quick css fix

--- app/aboutUs/OurTeam.tsx
+++ app/aboutUs/OurTeam.tsx
@@ -83,9 +83,9 @@ const OurTeam = () => {
               </div>
               <p>{label.description}</p>
               <div className="flex flex-row space-x-4">
-                <Linkedin className="icon--hover-scale"></Linkedin>
-                <X className="icon--hover-scale"></X>
-                <Instagram className="icon--hover-scale"></Instagram>
+                <Linkedin className="hover-scale"></Linkedin>
+                <X className="hover-scale"></X>
+                <Instagram className="hover-scale"></Instagram>
               </div>
             </div>
           </div>
