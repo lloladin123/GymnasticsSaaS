5090849cbb5da78d9f176814b4941de1b844d341
lloladin123 <blackiealexnader@yahoo.dk>
2025-05-18 02:07:15 +0200

    Rename images to trigger vercel update, due to linux vs windows case sensetivity

--- components/CommunityHightligt.tsx
+++ components/CommunityHightligt.tsx
@@ -8,7 +8,7 @@ const CommunityHightligt = () => {
       <div className="relative w-full md:w-6/12 h-64 md:h-128 bg-gray-100">
         <Image
           fill
-          src="/images/SpringHallen.png"
+          src="/images/SpringHallen1.png"
           alt="CommunityHightlight Image"
           className="object-contain"
         />
