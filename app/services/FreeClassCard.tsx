b76de9e4928942b4915b9f4a543b7a1b488d0bd7
lloladin123 <blackiealexnader@yahoo.dk>
2025-05-18 11:25:32 +0200

    add: Mobile support for ServicePage and all it's components

--- app/services/FreeClassCard.tsx
+++ app/services/FreeClassCard.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const FreeClassCard = () => {
   return (
-    <section className="mt-10 grid grid-cols-2 p-10">
+    <section className="mt-10 grid grid-rows-1 md:grid-cols-2 p-10">
       <div className="flex flex-col space-y-4">
         <h2 className="text-2xl font-black">Join Us for a Free Class!</h2>
         <p>Experience the joy of movement with us today!</p>
       </div>
-      <div className="flex items-end justify-end">
+      <div className="flex items-center justify-center md:items-end md:justify-end">
         <Link className="btn btn--black" href="#">
           SignUp
         </Link>
