684062a57fe99d190bf3d42409dcbeaec763bb2e
lloladin123 <blackiealexnader@yahoo.dk>
2025-07-01 00:46:19 +0200

    Refractor: Buttons

--- app/services/CtaServices.tsx
+++ app/services/CtaServices.tsx
@@ -1,22 +1,36 @@
-import Link from "next/link";
+import BasicButton from "@/components/Buttons/BasicButton";
 import React from "react";
+
+interface ICtaServicesText {
+  label: string;
+  heading: string;
+  description: string;
+  primaryCta: string;
+  secondaryCta: string;
+}
+
+const ctaServicesText: ICtaServicesText = {
+  label: "Empower",
+  heading: "Explore our Services",
+  description:
+    "Discover a diverse range of activities designed to inspire and engage every member of our community",
+  primaryCta: "Learn More",
+  secondaryCta: "Sign Up",
+};
 
 const CtaServices = () => {
   return (
     <section className="flex flex-col space-y-8 p-10">
-      <span>Empower</span>
-      <h2 className="text-4xl font-black">Explore our Services</h2>
-      <p>
-        Discover a diverse range of activities designed to inspire and engage
-        every member of our community
-      </p>
+      <span>{ctaServicesText.label}</span>
+      <h2 className="text-4xl font-black">{ctaServicesText.heading}</h2>
+      <p>{ctaServicesText.description}</p>
       <div className="flex flex-row space-x-4">
-        <Link className="btn btn--black" href="#">
-          Learn More
-        </Link>
-        <Link className="btn btn--ghost-black" href="#">
-          Sign Up
-        </Link>
+        <BasicButton variant="black" href="#">
+          {ctaServicesText.primaryCta}
+        </BasicButton>
+        <BasicButton variant="ghost-black" hoverEffect="positive" href="#">
+          {ctaServicesText.secondaryCta}
+        </BasicButton>
       </div>
     </section>
   );
